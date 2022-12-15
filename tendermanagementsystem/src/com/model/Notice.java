7c731c77f8065127e00c0690f1cdade82c93d711
SurajGundle <shubhamgundle35@gmail.com>
2022-12-15 10:15:48 +0530

    set up with model package

---
+++ tendermanagementsystem/src/com/model/Notice.java
@@ -0,0 +1,52 @@
+package com.model;
+
+public class Notice {
+	
+	private int noticeId;
+	private String noticeTitle;
+	private String noticeInfo;
+	
+	public Notice() {
+		
+	}
+
+	public Notice(int noticeId, String noticeTitle, String noticeInfo) {
+		super();
+		this.noticeId = noticeId;
+		this.noticeTitle = noticeTitle;
+		this.noticeInfo = noticeInfo;
+	}
+
+	public int getNoticeId() {
+		return noticeId;
+	}
+
+	public void setNoticeId(int noticeId) {
+		this.noticeId = noticeId;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+
+	public String getNoticeInfo() {
+		return noticeInfo;
+	}
+
+	public void setNoticeInfo(String noticeInfo) {
+		this.noticeInfo = noticeInfo;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [noticeId=" + noticeId + ", noticeTitle=" + noticeTitle + ", noticeInfo=" + noticeInfo + "]";
+	}
+	
+	
+	
+
+}
---
+++ tendermanagementsystem/src/com/model/Tender.java
@@ -0,0 +1,93 @@
+package com.model;
+
+import java.sql.Date;
+
+public class Tender {
+	
+	private String tid;
+	private String tname;
+	private String ttype;
+	private int tprice;
+	private String tdesc;
+	private Date tdeadline;
+	private String tlocation;
+	
+	public Tender() {
+		
+	}
+
+	public Tender(String tid, String tname, String ttype, int tprice, String tdesc, Date tdeadline, String tlocation) {
+		super();
+		this.tid = tid;
+		this.tname = tname;
+		this.ttype = ttype;
+		this.tprice = tprice;
+		this.tdesc = tdesc;
+		this.tdeadline = tdeadline;
+		this.tlocation = tlocation;
+	}
+
+	public String getTid() {
+		return tid;
+	}
+
+	public void setTid(String tid) {
+		this.tid = tid;
+	}
+
+	public String getTname() {
+		return tname;
+	}
+
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+
+	public String getTtype() {
+		return ttype;
+	}
+
+	public void setTtype(String ttype) {
+		this.ttype = ttype;
+	}
+
+	public int getTprice() {
+		return tprice;
+	}
+
+	public void setTprice(int tprice) {
+		this.tprice = tprice;
+	}
+
+	public String getTdesc() {
+		return tdesc;
+	}
+
+	public void setTdesc(String tdesc) {
+		this.tdesc = tdesc;
+	}
+
+	public Date getTdeadline() {
+		return tdeadline;
+	}
+
+	public void setTdeadline(Date tdeadline) {
+		this.tdeadline = tdeadline;
+	}
+
+	public String getTlocation() {
+		return tlocation;
+	}
+
+	public void setTlocation(String tlocation) {
+		this.tlocation = tlocation;
+	}
+
+	@Override
+	public String toString() {
+		return "Tender [tid=" + tid + ", tname=" + tname + ", ttype=" + ttype + ", tprice=" + tprice + ", tdesc="
+				+ tdesc + ", tdeadline=" + tdeadline + ", tlocation=" + tlocation + "]";
+	}
+	
+
+}
