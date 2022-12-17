4da9f72bfa461a38ffb451334fdf7de5aac617ca
SurajGundle <shubhamgundle35@gmail.com>
2022-12-17 21:02:22 +0530

    day2

--- tendermanagementsystem/src/com/tsm/model/Notice.java
+++ tendermanagementsystem/src/com/tsm/model/Notice.java
@@ -1,5 +1,53 @@
 package com.tsm.model;
 
+
 public class Notice {
+	
+	private int noticeId;
+	private String noticeTitle;
+	private String noticeInfo;
+	
+	public Notice() {
+		
+	}
+
+	public Notice(int noticeId, String noticeTitle, String noticeInfo) {
+		super();
+		this.noticeId = noticeId;
+		this.noticeTitle = noticeTitle;
+		this.noticeInfo = noticeInfo;
+	}
+
+	public int getNoticeId() {
+		return noticeId;
+	}
+
+	public void setNoticeId(int noticeId) {
+		this.noticeId = noticeId;
+	}
+
+	public String getNoticeTitle() {
+		return noticeTitle;
+	}
+
+	public void setNoticeTitle(String noticeTitle) {
+		this.noticeTitle = noticeTitle;
+	}
+
+	public String getNoticeInfo() {
+		return noticeInfo;
+	}
+
+	public void setNoticeInfo(String noticeInfo) {
+		this.noticeInfo = noticeInfo;
+	}
+
+	@Override
+	public String toString() {
+		return "Notice [noticeId=" + noticeId + ", noticeTitle=" + noticeTitle + ", noticeInfo=" + noticeInfo + "]";
+	}
+	
+	
+	
 
 }
--- tendermanagementsystem/src/com/tsm/model/TenderStatus.java
+++ tendermanagementsystem/src/com/tsm/model/TenderStatus.java
@@ -1,5 +1,62 @@
 package com.tsm.model;
 
 public class TenderStatus {
+	
+	private String tstendorId;
+	private String tsbidderId;
+	private String tsstatus;
+	private String tsvendorId;
+	
+	public TenderStatus() {
+		
+	}
+
+	public TenderStatus(String tstendorId, String tsbidderId, String tsstatus, String tsvendorId) {
+		super();
+		this.tstendorId = tstendorId;
+		this.tsbidderId = tsbidderId;
+		this.tsstatus = tsstatus;
+		this.tsvendorId = tsvendorId;
+	}
+
+	public String getTstendorId() {
+		return tstendorId;
+	}
+
+	public void setTstendorId(String tstendorId) {
+		this.tstendorId = tstendorId;
+	}
+
+	public String getTsbidderId() {
+		return tsbidderId;
+	}
+
+	public void setTsbidderId(String tsbidderId) {
+		this.tsbidderId = tsbidderId;
+	}
+
+	public String getTsstatus() {
+		return tsstatus;
+	}
+
+	public void setTsstatus(String tsstatus) {
+		this.tsstatus = tsstatus;
+	}
+
+	public String getTsvendorId() {
+		return tsvendorId;
+	}
+
+	public void setTsvendorId(String tsvendorId) {
+		this.tsvendorId = tsvendorId;
+	}
+
+	@Override
+	public String toString() {
+		return "TenderStatus [tstendorId=" + tstendorId + ", tsbidderId=" + tsbidderId + ", tsstatus=" + tsstatus
+				+ ", tsvendorId=" + tsvendorId + "]";
+	}
+	
+	
 
 }
